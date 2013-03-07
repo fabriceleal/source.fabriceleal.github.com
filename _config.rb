43b60c5473efd41905644c735a736348dfa0db69
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-03-07 15:43:56 -0800

    reading raw configurations, writing to new file (not including it, testing!)

--- _config.rb
+++ _config.rb
@@ -2,14 +2,12 @@
 
 require 'yaml'
 
-#raw = YAML.load_file('_raw_config.yml')
-#puts raw.inspect
 
-data = {
-  :safe => false,
-  :source => ".",
-  :lsi => false
-}
+data = YAML.load_file('_raw_config.yml')
+
+# TODO
+# Automatic stuff. Read projects from github.
+
 
 File.open("_config_test.yml", "w") do |f|
   f.write(data.to_yaml)
